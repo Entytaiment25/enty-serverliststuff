16286dd7dc3712598b8ec7e1295455db066fafda
Louis Anthony Darr <64799287+Entytaiment25@users.noreply.github.com>
2022-10-08 21:52:28 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Fennected Solution Group - enty'
+description 'funny description here'
+version '0.0.1'
+discord 'soon...'
+
+server_script 'server.lua'
+
+resource_type 'gametype' { name = 'Roleplay' } -- change the name string
+
+lua54 'yes'
---
+++ server.lua
@@ -0,0 +1,3 @@
+-- Script Header --
+SetMapName("Sand Andreas") -- set the map name displayed on the fivem serverlist
+SetConvarServerInfo("Info", "This is some text lol") -- set the details/information displayed on the fivem serverlist
