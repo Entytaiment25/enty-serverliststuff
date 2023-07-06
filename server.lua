2e8562af798fb7738fc62e6cab41c17458068695
enty <64799287+Entytaiment25@users.noreply.github.com>
2023-07-07 00:12:44 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,3 @@
 -- Script Header --
-SetMapName("Sand Andreas") -- set the map name displayed on the fivem serverlist
+SetMapName("San Andreas") -- set the map name displayed on the fivem serverlist, fuck you tranq lmao 😻
 SetConvarServerInfo("Info", "This is some text lol") -- set the details/information displayed on the fivem serverlist
